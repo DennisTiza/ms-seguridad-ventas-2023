055ebd9cc14be9fe321ff57539b45973d44ac689
DennisTiza <dennis.tisalema30646@ucaldas.edu.co>
2023-10-18 20:38:42 -0500

    Variables de entorno en Dotnetcore y loopback

--- src/application.ts
+++ src/application.ts
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
+import {AuthenticationComponent, registerAuthenticationStrategy} from '@loopback/authentication';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
-import {MySequence} from './sequence';
-import {AuthenticationComponent, registerAuthenticationStrategy} from '@loopback/authentication';
 import {AuthStrategy} from './auth/strategy';
+import {MySequence} from './sequence';
 
 
 export {ApplicationConfig};
--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,10 +1,11 @@
 export namespace ConfiguracionSeguridad {
-  export const claveJWT: string = "Admin@2023*"
+  export const claveJWT = process.env.SECRET_PASSWORD_JWT;
   export const menuUsuarioId = "65077d1425eb9125c09d4c2d";
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
   export const eliminarAccion = "eliminar";
   export const descargarAccion = "descargar";
+  export const mongodbConnectionString = process.env.CONNECTION_STRING_MONGODB
 
 }
